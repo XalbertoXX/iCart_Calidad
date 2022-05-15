a00665dc59c61ee382f6a21146bbeede332d46db
XalbertoXX <albertoxx1998@hotmail.com>
2022-05-15 21:30:06 +0200

    Cambios Funcionalidad Feedback

--- app/src/main/java/com/example/icartfinal/CategoriesActivity.kt
+++ app/src/main/java/com/example/icartfinal/CategoriesActivity.kt
@@ -14,16 +14,23 @@ class CategoriesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_categories)
 
         val btnVerLista = findViewById<Button>(R.id.btnVerListaCat)
-
+        val btnGoToFeedBack = findViewById<Button>(R.id.GoToFeedBack)
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewCategorias)
         val adaptador = CategoryAdapter(ShoppingList.listadoCategorias, this)
+
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerView.adapter = adaptador
         recyclerView.setHasFixedSize(true)
+
         btnVerLista.setOnClickListener {
             val intent = Intent(this, ShoppingListActivity::class.java)
             startActivity(intent)
         }
+        btnGoToFeedBack.setOnClickListener {
+            val intent = Intent(this, FeedBackActivity::class.java)
+            startActivity(intent)
+        }
+
         val numProduct = findViewById<TextView>(R.id.numProducts)
         numProduct.text = ShoppingList.listaCompra.size.toString()
     }
---
+++ app/src/main/java/com/example/icartfinal/FeedBackActivity.kt
@@ -0,0 +1,26 @@
+package com.example.icartfinal
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class FeedBackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feedback)
+
+        val btnIntroText = findViewById<EditText>(R.id.editTextTextMultiLine)
+        val btnSubmit = findViewById<Button>(R.id.submit_Feedback)
+
+        btnIntroText.setOnClickListener { btnIntroText.text = btnIntroText.text }
+
+        btnSubmit.setOnClickListener {
+            val intent = Intent(this, ShoppingListActivity::class.java)
+            Toast.makeText(this, "Submit Sent :D", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+        }
+    }
+}
